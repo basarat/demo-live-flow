4656c18e147812add92a0633869832cd6222c921
basarat <basaratali@gmail.com>
2022-05-16 09:42:24 +1000

    wrap

---
+++ src/api.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+export const api = Router();
+
+type Payload = {
+  message: string,
+};
+
+let payload: Payload = {
+  "message": "Like && Subscribe 🌹"
+};
+
+api.get('/', (_req, res) => {
+  res.send(payload);
+});
