343ac64ac447d657c3310db49815fb6f7af1a4b0
basarat <basaratali@gmail.com>
2022-05-15 13:06:09 +1000

    init

--- src/api.ts
+++ src/api.ts
@@ -7,21 +7,9 @@ type Payload = {
 };
 
 let payload: Payload = {
-  "message": "Like && Subscribe"
+  "message": "Like"
 };
 
 api.get('/', (_req, res) => {
-  res.send(payload);
-});
-
-api.post('/', (req, res) => {
-  const payloadNext: Payload = req.body;
-  if (!payloadNext.message) {
-    res.status(400).send({
-      error: 'Invalid request: No message property',
-    });
-    return;
-  }
-  payload = payloadNext;
   res.send(payload);
 });
